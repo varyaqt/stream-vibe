48f744c1e0dc726fc2a9e87a252b2098ff58d20d
popadina varvara <popadinavar@ya.ru>
2025-04-21 22:25:24 +0300

    start to developing Tabs + readme

---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,49 @@
+import classNames from "classnames"
+import "./Tabs.scss"
+import getTabsElementsIdsFromTitle from "./utils/getTabsElementsIdsFromTitle"
+import TabsNavigation from "./components/TabsNavigation"
+
+const Tabs = (props) => {
+  const {
+    className,
+    title,
+    items = [],
+    navigationTargerElementId = null,
+  } = props
+
+  const { buttonId, contentId } = getTabsElementsIdsFromTitle(title)
+
+  return (
+    <div
+      className={classNames(className, "tabs")}
+      data-js-tabs={JSON.stringify({
+        navigationTargerElementId,
+      })}
+    >
+      {!navigationTargerElementId && (
+        <TabsNavigation title={title} items={items} />
+      )}
+      <div className="tabs__body">
+        {items.map((item, index) => {
+          const { title, children, isActive } = item
+          return (
+            <div
+              className={classNames("tabs__content", {
+                "is-active": isActive,
+              })}
+              id={contentId}
+              aria-labelledby={buttonId}
+              tabIndex={0}
+              data-js-tabs-content=""
+              key={index}
+            >
+              {children}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Tabs
