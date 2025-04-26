5e2934614e62090f9ebc6c99435a7ad6f3417d96
popadina varvara <popadinavar@ya.ru>
2025-04-26 22:16:52 +0300

    realizing BaseComponent (abstract class)

--- src/modules/TabsCollection .js
+++ src/modules/TabsCollection .js
@@ -1,9 +1,10 @@
 import getParams from "@/utils/getParams"
 import pxToRem from "@/utils/pxToRem"
+import BaseComponent from "./generic/BaseComponent"
 
 const rootSelector = "[data-js-tabs]"
 
-class Tabs {
+class Tabs extends BaseComponent {
   selectors = {
     root: rootSelector,
     navigation: "[data-js-tabs-navigation]",
@@ -21,6 +22,7 @@ class Tabs {
   }
 
   constructor(rootElement) {
+    super()
     this.rootElement = rootElement
     this.params = getParams(this.rootElement, this.selectors.root)
     this.navigationElement = this.params.navigationTargetElementId
@@ -32,11 +34,11 @@ class Tabs {
     this.contentElements = [
       ...this.rootElement.querySelectorAll(this.selectors.content),
     ]
-    this.state = {
+    this.state = this.getProxyState({
       activeTabIndex: this.buttonElements.findIndex(
         ({ ariaSelected }) => ariaSelected
       ),
-    }
+    })
     this.limitTabsIndex = this.buttonElements.length - 1
     this.bindEvents()
     setTimeout(this.bindObservers, 500)
@@ -83,7 +85,6 @@ class Tabs {
 
   activateTab(newTabIndex) {
     this.state.activeTabIndex = newTabIndex
-    this.updateUI()
     this.buttonElements[newTabIndex].focus()
   }
 
@@ -115,7 +116,6 @@ class Tabs {
 
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex
-    this.updateUI()
   }
 
   onKeyDown = (event) => {
